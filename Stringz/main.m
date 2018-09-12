9ae941ecd3e9a300b373166120c18fd2c6b4be6f
Yasser Altamimi <yosy2010@gmail.com>
2018-09-13 02:57:30 +0300

    Initial Commit

---
+++ Stringz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by YASSER ALTAMIMI on 03/01/1440 AH.
+//  Copyright © 1440 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
